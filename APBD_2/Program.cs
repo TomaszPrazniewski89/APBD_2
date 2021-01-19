4efca4491256f3086caf212c5a3406504a11a213
tomasz prażniewski <tomaszprazniewski89@gmail.com>
2021-01-19 11:56:30 +0100

    add

--- APBD_2/Program.cs
+++ APBD_2/Program.cs
@@ -1,5 +1,11 @@
-﻿using System;
+﻿using APBD_2.Classes; //inna siezka gdyz zmienilem nazwe z Classes na Models
+using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
+using System.Linq;
+using System.Xml.Serialization;
 
 namespace APBD_2
 {
@@ -7,17 +13,34 @@ namespace APBD_2
     {
         static void Main(string[] args)
         {
-            //Getting the parameters
+            //1. Getting the parameters
 
 
 
-            //Loading the data from CSV
+            //2. Loading the data from CSV
             // te klasy za pomoca using wyzwalaja metode dispose( ktora zwalnia zasoby)
             string args1 = args[0];
-            StreamReader reader = new StreamReader("dane.csv");
-            //CsvReader scv = new CsvReader(reader,CultureInfo.InvariantCulture)
+            using (StreamReader reader = new StreamReader("dane.csv"))
+            using (CsvReader csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                Student student = new Student();
+                //powiadomienie biblioteki ze pierwszy naglowek nie zawiera nazwl kolumn
+                csv.Configuration.HasHeaderRecord = false;
+                                        //metoda ktora wczytuje dane i probouje je parsowac na kalse Student, (ToList) wynik w postaci kolekci typu lista
+                List<Student> records = csv.GetRecords<Student>().ToList();
 
-            Console.WriteLine("Hello World!");
+            }
+
+            //3. Save CSV into XML
+
+            FileStream writer = new FileStream(@"data.xml", FileMode.Create);
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Student>), new XmlRootAttribute("uczelnia"));
+            serializer.Serialize(writer, records);
+            writer.Close();
+
+            //4. Save CSV into json
+            string jsonString = System.Text.Json.JsonSerializer.Serialize(records);
+            File.WriteAllText("data.json", jsonString);
         }
     }
 }
